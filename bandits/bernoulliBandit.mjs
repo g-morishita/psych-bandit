cec0c681baeb732dffbc11184b3acc57801db7ed
Gota Morishita <optimistic.gota@gmail.com>
2023-10-16 13:11:51 +1100

    add comments to bandit module

--- bandits/bernoulliBandit.mjs
+++ bandits/bernoulliBandit.mjs
@@ -1,46 +1,75 @@
 import { Bandit } from "./bandit.mjs";
 
+/**
+ * StableBernoulliBandit represents a multi-armed bandit problem
+ * where each arm provides a reward based on a Bernoulli distribution.
+ * The probability of reward for each arm remains stable over time.
+ */
 class StableBernoulliBandit extends Bandit {
+  /**
+   * Constructs a StableBernoulliBandit instance with specified reward probabilities for each arm.
+   *
+   * @param {number[]} rewardProbabilities - An array representing the probability of reward for each arm.
+   *                                         Each probability should be a value between 0 and 1.
+   *                                         The array should contain at least two probabilities.
+   */
   constructor(rewardProbabilities) {
     super();
-    this.rewardProbabilities = rewardProbabilities;
-  }
-  get rewardProbabilities() {
-    return this._rewardProbabilities;
-  }
-  set rewardProbabilities(rewardProbabilities) {
-    if (typeof this.rewardProbabilities !== "undefined") {
-      throw new Error("You can't change the mean reward.");
-    }
-    // Check the length of the reward probabilities is larger than 1.
+
+    // Ensure there are at least two arms
     if (rewardProbabilities.length <= 1) {
       throw new Error(
-        `The length of the meanRewards is less than 2. Check the given param: ${rewardProbabilities}`
+        `The length of rewardProbabilities must be greater than 1. Given: ${rewardProbabilities}`
       );
     }
-    // Check the reward probability is in a valid range [0, 1].
+
+    // Validate each reward probability is in the range [0, 1]
     for (const rewardProb of rewardProbabilities) {
-      if (rewardProb < 0 && rewardProb > 1) {
+      if (rewardProb < 0 || rewardProb > 1) {
         throw new Error(
-          `Reward probability must be somewhere between 0 and 1. Given ${rewardProb}`
+          `Each reward probability must be between 0 and 1. Given: ${rewardProb}`
         );
       }
     }
+
     this._rewardProbabilities = rewardProbabilities;
   }
 
+  /**
+   * Get the reward probabilities for each arm.
+   *
+   * @returns {number[]} An array of reward probabilities for each arm.
+   */
+  get rewardProbabilities() {
+    return this._rewardProbabilities;
+  }
+
+  /**
+   * Setter to prevent changes to reward probabilities after initialization.
+   *
+   * @param {number[]} _ - Ignored parameter. Used to throw an error when attempting to set rewardProbabilities.
+   * @throws {Error} If an attempt is made to change reward probabilities.
+   */
+  set rewardProbabilities(_) {
+    throw new Error(
+      "You can't change the reward probabilities after initialization."
+    );
+  }
+
+  /**
+   * Determine the reward for a chosen arm. The reward is 1 or 0 based on a Bernoulli distribution.
+   *
+   * @param {number} madeChoice - The index of the chosen arm.
+   * @returns {number} Returns 1 if the random draw is less than the reward probability of the chosen arm, otherwise returns 0.
+   * @throws {Error} If the chosen arm index is invalid.
+   */
   getReward(madeChoice) {
-    const randomVal = Math.random();
     if (madeChoice >= this._rewardProbabilities.length) {
-      throw new Error(
-        `The index of arm exceeds the given reward vector. Given ${madeChoice}`
-      );
+      throw new Error(`Invalid arm index: ${madeChoice}.`);
     }
+
     const rewardProbability = this._rewardProbabilities[madeChoice];
-    if (rewardProbability > randomVal) {
-      return 1;
-    }
-    return 0;
+    return Math.random() < rewardProbability ? 1 : 0;
   }
 }
 
